cf3ef624160ff19c315b734c42eb001e3263188d
Raifa21 <helloiamkotaro@gmail.com>
2025-09-13 16:59:53 +0700

    feat: add user modal display logic to prompt for username if not logged in

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
   import './app.css';
   import MainPage from './lib/pages/MainPage.svelte';
   import BookPage from './lib/pages/BookPage.svelte';
+  import UsernameModal from '$lib/components/UsernameModal.svelte';
+  import { user } from '$lib/stores/user';
+  import { get } from 'svelte/store';
   
   let route: 'main' | 'book' = 'main';
   let bookId: string | null = null;
@@ -20,10 +23,21 @@
   
   parseHash();
   window.addEventListener('hashchange', parseHash);
+
+  let showUserModal = false;
+  function checkUser() {
+    const u = get(user);
+    showUserModal = !u.userId;
+  }
+  checkUser();
 </script>
 
 {#if route === 'main'}
   <MainPage />
 {:else if route === 'book' && bookId}
   <BookPage bookId={bookId} />
+{/if}
+
+{#if showUserModal}
+  <UsernameModal on:submitted={() => (showUserModal = false)} />
 {/if}
